3f062393cc28b188a3c8f098678c9cc988d753a1
Zohre Abbasi <abz45don98@gmail.com>
2026-09-09 21:44:09 +0330

    add routing and pages

--- zohre-abbasi-week19/admin-panel/src/App.jsx
+++ zohre-abbasi-week19/admin-panel/src/App.jsx
@@ -1,9 +1,19 @@
-
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProductsPage from "./pages/ProductsPage";
 
 function App() {
   return (
-    <div>Admin-panel</div>
-  )
+    <div>
+      <h1>Admin pages</h1>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
